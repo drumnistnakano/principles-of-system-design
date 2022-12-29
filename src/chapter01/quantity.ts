53e9765a827149c2d71fb027f1a44a7f68c0dfa3
Yoshiyuki Nakano <syxqh349ysykn@gmail.com>
2022-12-29 09:09:56 +0900

    quantity.tsのエラー修正

--- src/chapter01/quantity.ts
+++ src/chapter01/quantity.ts
@@ -1,26 +1,31 @@
+type otherValue = {
+  value: number
+}
+
 interface quantity {
   value: number
-  canAdd(other: number): boolean
-  add(other: number): number
-  addValue(other: number): number
+  canAdd(other: otherValue): boolean
+  add(other: otherValue): number
+  addValue(other: otherValue): number
 }
 
 class Quantity implements quantity {
   private MIN: number = 1
   private MAX: number = 100
+  value: number
 
-  constructor(private value: number) {
-    if (value < this.MIN) throw new Error(`不正: ${this.MIN} 未満`)
-    if (value > this.MAX) throw new Error(`不正 ${this.MAX} 超`)
-    this.value = value
+  constructor(_value: number) {
+    if (_value < this.MIN) throw new Error(`不正: ${this.MIN} 未満`)
+    if (_value > this.MAX) throw new Error(`不正 ${this.MAX} 超`)
+    this.value = _value
   }
 
-  canAdd = (other: quantity): boolean => {
+  canAdd(other: otherValue): boolean {
     const added = this.addValue(other)
     return added <= this.MAX
   }
 
-  add = (other: quantity): Quantity => {
+  add(other: otherValue): number {
     if (!this.canAdd(other)) throw new Error(`不正:合計が ${this.MAX} 超`)
 
     const added = this.addValue(other)
@@ -28,12 +33,13 @@ class Quantity implements quantity {
     return new Quantity(added)
   }
 
-  private addValue = (other: quantity): number => {
+  private addValue(other: otherValue): number {
     return this.value + other.value
   }
 }
 
 const quantity = new Quantity(2)
 console.log(quantity)
+console.log(quantity.add(3))
 
 export {}
