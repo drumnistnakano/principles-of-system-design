4d3d4a78ee27423aa35b22bec4061d281fd95e75
Yoshiyuki Nakano <syxqh349ysykn@gmail.com>
2023-01-07 18:46:06 +0900

    testname誤字修正

--- test/chapter01/quantity.test.ts
+++ test/chapter01/quantity.test.ts
@@ -1,6 +1,6 @@
 import { Quantity } from '../../src/chapter01/quantity'
 
-test('quantity test, 1 + 2 to equail 3', () => {
+test('quantity test, 1 + 2 to equal 3', () => {
   const quantity = new Quantity(1)
   const added = new Quantity(2)
   const { value } = quantity.add(added)
